e7a63028e8a2a3778e6ee85d50cea869159e3ad1
ErickSebas <ume0028566@est.univalle.edu>
2022-11-07 02:54:52 -0400

    Arreglos

--- src/app/component/checkout/checkout.component.ts
+++ src/app/component/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/service/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -12,7 +13,7 @@ export class CheckoutComponent implements OnInit {
   public first_name : any = sessionStorage.getItem('name');
   public last_name : any = sessionStorage.getItem('lname');
   public email : any = sessionStorage.getItem('email');
-  constructor(private cartService : CartService) { }
+  constructor(private cartService : CartService, private router : Router) { }
 
   ngOnInit(): void {
     this.cartService.getProducts()
@@ -25,6 +26,7 @@ export class CheckoutComponent implements OnInit {
   checkoutGo(){
     alert("Your order has been placed, check your email for details");
     this.cartService.removeAllCart();
+    this.router.navigate(['cart']);
   }
 
 }
