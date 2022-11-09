8a890350df4e345b740ea7935c71a79c8072e216
AnthonyVocal <anthonyvocalhurtado@gmail.com>
2022-11-09 10:51:20 -0400

    Vista detalle producto apartir de un click

--- src/app/component/detailproduct/detailproduct.component.ts
+++ src/app/component/detailproduct/detailproduct.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { ProductsComponent } from 'src/app/component/products/products.component';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-detailproduct',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailproductComponent implements OnInit {
 
-  constructor() { }
+  public detailList: any;
+  public filterCategory: any;
+  public product: any;
+  constructor(private api: ApiService, private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.api.getProduct()
+      .subscribe(res => {
+        this.detailList = res;
+        this.filterCategory = res;
+        this.detailList.map((a: any, index: any) => {
+
+          console.log(a.id);
+          if (a.id == sessionStorage.getItem('detailid')) {
+            this.product = a;
+
+          }
+        });
+      });
+
+  }
+
+  addtocart(item: any) {
+    this.cartService.addtoCart(item);
   }
 
 }
